7dffb3a82d4e3ba17e60bb7ee6b0bf23e73457df
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-04-08 15:32:30 +0530

    webot client

---
+++ script.js
@@ -0,0 +1,127 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval
+
+//LOADING  OF ANSWERS//
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        
+        element.textContent += '.';
+
+                if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+
+//typing one letter at once functionality that too under a specific index//
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 20)
+}
+
+
+//generating unique id for every msg//
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+
+//display of diff logos and diff color of chatstripe for user and ai//
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+ 
+//to get ai generated response
+const handleSubmit = async(e) => {
+    e.preventDefault()
+    const data = new FormData(form);
+
+    //user stripe
+chatContainer.innerHTML += chatStripe(false, data.get('prompt'))   
+form.reset();
+
+//bot stripe
+const uniqueId = generateUniqueId();
+chatContainer.innerHTML += chatStripe(true, " " , uniqueId);
+
+chatContainer.scrollTop = chatContainer.scrollHeight;
+
+const messageDiv = document.getElementById(uniqueId)
+loader(messageDiv);
+
+
+
+
+//*//FETCHING DATA FROM SERVER//*//
+
+const response = await fetch('https://webotserver.onrender.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " ";
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
+}
+
+
+ //PRESSING THE ENTER KEY MEANS SUBMIT//
+ form.addEventListener('submit', handleSubmit)
+ form.addEventListener('keyup', (e) => {
+     if (e.keyCode === 13) {
+         handleSubmit(e)
+     }
+ })
+
+
+ 
